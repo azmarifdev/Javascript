22df366753c93c00c343ec188864d8fe90a87428
azmarifdev <azmarif.dev@gmail.com>
2024-01-09 03:57:07 +0600

    string

--- array/array.js
+++ array/array.js
@@ -8,5 +8,16 @@ const details = [];
 details[0] = 'name';
 details[1] = 'username';
 console.log(details);
-
 console.log(typeof cars);
+console.log(cars.length);
+console.log(cars.length - 1);
+console.log(cars[cars.length - 1]);
+
+const fruits = ['banana', 'orange', 'apple', 'mango'];
+console.log(fruits.indexOf('mango'));
+// fruits[ 3 ] = "fol";
+console.log(fruits);
+console.log((fruits[ 4 ] = 'tomato'));
+console.log(fruits);
+console.log(fruits[ 3 ]);
+console.log(fruits[fruits.length -1]);
---
+++ string/string.js
@@ -0,0 +1,6 @@
+let text = 'HELLO WORLD';
+console.log((char = text.at(0)));
+console.log((char = text.charAt(0)));
+console.log((char = text.charCodeAt(0)));
+
+
