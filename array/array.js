5d236231de25bf1f908a34348460ee01fb9d5979
azmarifdev <azmarif.dev@gmail.com>
2024-01-09 00:27:09 +0600

    js

--- array/array.js
+++ array/array.js
@@ -1,1 +1,12 @@
-const cars = [ "saab", "volvo", "BMW" ];
+const cars = ['saab', 'volvo', 'BMW'];
+console.log(typeof cars);
+console.log(cars.length);
+console.log(cars[2]);
+console.log(cars.indexOf('saab'));
+
+const details = [];
+details[0] = 'name';
+details[1] = 'username';
+console.log(details);
+
+console.log(typeof carsk);
